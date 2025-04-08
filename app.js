3a653a15f06459c606b08a352917690afbcf822c
[BelloOlajide] <[belloolajide138@gmail.com]>
2025-04-08 08:32:43 +0100

    modify phase 2

--- app.js
+++ app.js
@@ -14,9 +14,13 @@ function appendToDisplay(value) {
 
 function calculate() {
     try {
+
         display.value = eval(display.value);
         result = display.value;
-    } catch (error) {
+
+    } 
+    
+    catch (error) {
         display.value = "Error";
         clearDisplay();
     }
@@ -26,7 +30,23 @@ function clearDisplay() {
     display.value = "0";
 }
 
-// function to remove the last digit from the display
+// function to remove the last digit from the display slice(0, -1) 0 for starting and -1 for the begining of the end. this specifies the begining of string and tells the system to start slicing from the end
+
 function removeDigitFromInput() {
    display.value = display.value.slice(0, -1);
+
+//    display.value = display.value === '' ? '0' : display.value;
+
+if (display.value === '') {
+// if display.value is equivalent to empty, set it to zero
+    display.value = "0";
+
+} 
+
+else {
+// else let the last digit be the last inputed digit
+    display.value = display.value;
+
+}
+
 }
